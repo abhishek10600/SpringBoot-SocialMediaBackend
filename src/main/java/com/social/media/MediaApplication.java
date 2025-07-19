d39e473d71dbe5cb045ee05bb00fe18df18c5b1d
abhishek10600 <abhi10sheksharma@gmail.com>
2025-07-19 22:12:52 +0530

    Create Data initializer in commandline and got rid of circular reference using JsonIgnore annotation. Create getUsers controller.

--- src/main/java/com/social/media/MediaApplication.java
+++ src/main/java/com/social/media/MediaApplication.java
@@ -3,6 +3,7 @@ package com.social.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MediaApplication {
 
